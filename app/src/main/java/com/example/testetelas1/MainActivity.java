65c49d0a90e4060e42b77743a866beae76d85f94
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2020-05-22 21:25:10 -0300

    Desenvolvendo o inicio das telas de teste

--- app/src/main/java/com/example/testetelas1/MainActivity.java
+++ app/src/main/java/com/example/testetelas1/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.testetelas1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView =  findViewById(R.id.link);
+       //textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                    enviaPerfil();
+            }
+        });
     }
+
+
+    public void enviaPerfil(){
+        Intent intent = new Intent(MainActivity.this, Perfil.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/testetelas1/Perfil.java
@@ -0,0 +1,19 @@
+package com.example.testetelas1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
